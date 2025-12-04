a80f39a0ad10d5fcd7b6961d35d28bf61256ce64
Erik <ewpeters@asu.edu>
2025-12-03 20:28:48 -0700

    fix custom factor slideability

--- display.js
+++ display.js
@@ -137,7 +137,7 @@
 
       let factorInput = document.getElementById("custom-factor")
       factorInput.addEventListener("change", (e) => {
-        this.customFactor = parseFloat(e.target.value);
+        this.customFactor = parseFloat(e.target.value) / 100;
       })
 
       // Slider logic
